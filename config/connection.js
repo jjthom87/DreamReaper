d2aee377fbfd594d06d17c56a4758358309570a6
Jared Thomas <jjthom87@yahoo.com>
2016-09-03 11:54:31 -0400

    first heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,38 @@
+// var Sequelize = require("sequelize");
+
+// var source = {
+
+// 	localhost: {
+// 		part: 3306,
+// 		host: 'localhost',
+// 		user: 'root',
+// 		password: "",
+// 		database: "dreamuser2"
+// 	},
+// 	jawsDB: {
+// 		port: 3306,
+// 		host: 'uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+// 		user: 'ejphf4ptfrzr9rq4',
+// 		password: 'xedib3xkud77fdoe',
+// 		database: 'h66tuvpi4nssri86'
+// 	}
+// }
+
+// var selectedSource - source.jawsDB;
+
+// var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
+// 	host: selectedSource.host,
+// 	dialect: 'mysql',
+
+// 	pool: {
+// 		max: 5,
+// 		min: 0,
+// 		idle: 10000
+// 	},
+// });
+
+// module.exports = sequelize;
+
 var Sequelize = require("sequelize");
 var mysql = require('mysql');
 
